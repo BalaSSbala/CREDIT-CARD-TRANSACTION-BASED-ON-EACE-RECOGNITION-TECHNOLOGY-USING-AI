575d4eb6b7138088bc4b4a344ba9382507a9c024
BalaSSbala <msbala1122@gmail.com>
2024-05-22 10:33:00 +0530

    files upload

---
+++ s.py
@@ -0,0 +1,17 @@
+
+# import mysql.connector
+# Bookingid=
+# def con():
+#     try:
+#         conn1 = mysql.connector.connect(user='bala', password='Bala@333!', host='localhost', database='creditcard')
+#         cursor = conn1.cursor()
+#         cursor.execute(
+#             "INSERT INTO booktb VALUES ('" + Bookingid + "','" + pid + "','" + new_string + "','" + UserName + "','" + Mobile + "','" + Email + "','" + str(
+#                 Qty) + "','" + str(Amount) + "','" + str(CardType) + "','" + str(CardNo) + "','" + str(
+#                 CvNo) + "','" + str(date) + "','0')")
+#         conn1.commit()
+#         conn1.close()
+#     except mysql.connector.Error as e:
+#         print("Error",e)
+
+# print("success",con())
